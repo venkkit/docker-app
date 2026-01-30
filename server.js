308d0db7d8de1f498ac62bf8764d4717edcc0e11
Ubuntu <ubuntu@ip-172-31-18-68.ec2.internal>
2026-01-30 08:05:11 +0000

    Initial dockerized app

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Docker container 🚀\n');
+}).listen(PORT);
+
+console.log(`Server running on port ${PORT}`);
