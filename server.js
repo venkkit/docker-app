de5b4c7f9cba48e8f6eddbf19d73debb4dbd28b9
Ubuntu <ubuntu@ip-172-31-18-68.ec2.internal>
2026-02-09 09:55:58 +0000

    Update app message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Docker container  to venkat for the 1st test🚀\n');
+  res.end('Hello from Docker container  to nikki for the 1st test🚀\n');
 }).listen(PORT);
 
 console.log(`Server running on port ${PORT}`);
