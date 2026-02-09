b6d2a9d1b9861c1e58e3e81b70e86ffa2faae3c4
Ubuntu <ubuntu@ip-172-31-18-68.ec2.internal>
2026-02-09 10:08:44 +0000

    Fix server to listen on 0.0.0.0

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Docker container  to nikki for the 1st test🚀\n');
-}).listen(PORT);
+  res.end('Hello from Docker container to nikki for the 1st test 🚀\n');
+}).listen(PORT, '0.0.0.0');   // 👈 IMPORTANT LINE
 
 console.log(`Server running on port ${PORT}`);
